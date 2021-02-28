b6596f706ab1cddef9d42d1aa1c64a2050b8b8a9
xd <n@10.10.10.11>
2021-02-28 22:10:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
+import time
 
 chromeDriverPath = "../chromedriver"
 driver = webdriver.Chrome(chromeDriverPath)
 
 
 uri = "https://pvp.giustizia.it/pvp/"
-
+address = "Bergamo, Province of Bergamo, Italy"
 
 driver.get(uri)
 
-//driver.quit()
+modalBtn = driver.find_element_by_class_name("bottoni-chiusura")
+modalBtn.click()
+
+time.sleep(5)
+
+driver.quit()
